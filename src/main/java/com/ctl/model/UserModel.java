ff107aa7b347f7a054168470722d607cf763f065
unknown <jaishankarpandey329@gmail.com>
2018-09-24 00:31:26 +0530

    adding dependency proerty

--- src/main/java/com/ctl/model/UserModel.java
+++ src/main/java/com/ctl/model/UserModel.java
@@ -1,6 +1,10 @@
 package com.ctl.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -9,6 +13,56 @@ public class UserModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", unique = true, nullable = false)
-	private integer id;
+	private Integer id;
+	private String entrydate,modifydate,firstname,lastname,phoneno,address1,address2;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getEntrydate() {
+		return entrydate;
+	}
+	public void setEntrydate(String entrydate) {
+		this.entrydate = entrydate;
+	}
+	public String getModifydate() {
+		return modifydate;
+	}
+	public void setModifydate(String modifydate) {
+		this.modifydate = modifydate;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	
 	
 }
